9c6d1e69899b0eb78f8e85d45906489cac0de6d8
Eros Benitez <67855699+Shinigamy19@users.noreply.github.com>
2026-01-12 16:10:50 -0300

    Certificados drive y link whatsapp

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -1,172 +1,196 @@
 import type { Course } from "../types/Course";
 
 export const coursesData: Course[] = [
-    {
-        title: "AWS Community Day Argentina 2025",
-        platform: "AWS Community",
-        year: "2025",
-        icon: "code",
-        certificateUrl: "/certificates/AWS Community Day Argentina 2025 - Certificado.pdf",
-    },
-    {
-        title: "Diseño web con HTML y CSS",
-        platform: "Certificado",
-        year: "2025",
-        icon: "html",
-        certificateUrl: "/certificates/Diseño web con HTML y CSS - Certificado.pdf",
-    },
-    {
-        title: "Introducción a Java",
-        platform: "Certificado",
-        year: "2025",
-        icon: "java",
-        certificateUrl: "/certificates/Introduccion a Java - Certificado.pdf",
-    },
-    {
-        title: "Introducción a Python",
-        platform: "Certificado",
-        year: "2025",
-        icon: "python",
-        certificateUrl: "/certificates/Introduccion a Python - Certificado.pdf",
-    },
-    {
-        title: "Programación con PHP y MySQL",
-        platform: "Certificado",
-        year: "2025",
-        icon: "php",
-        certificateUrl: "/certificates/Programacion con PHP y MYSQL - Certificado.pdf",
-    },
-    {
-        title: "Crear un portafolio para un Científico de Datos",
-        platform: "Bootcamp de Ciencia de Datos",
-        year: "2024",
-        icon: "code",
-        certificateUrl: "/certificates/Certificado -  Crear un portafolio para un Científico de Datos - Bootcamp de Ciencia de Datos .pdf",
-    },
-    {
-        title: "Redes Neuronales",
-        platform: "Bootcamp de Machine Learning",
-        year: "2024",
-        icon: "code",
-        certificateUrl: "/certificates/Certificado -  Redes Neuronales - Bootcamp de Machine Learning .pdf",
-    },
-    {
-        title: "Animaciones con JavaScript",
-        platform: "Bootcamp",
-        year: "2024",
-        icon: "js",
-        certificateUrl: "/certificates/Certificado - Animaciones con JavaScript - Bootcamp.pdf",
-    },
-    {
-        title: "Aprendiendo a usar Figma",
-        platform: "Bootcamp de Diseño de Interfaces y CSS",
-        year: "2024",
-        icon: "figma",
-        certificateUrl: "/certificates/Certificado - Aprendiendo a usar Figma - Bootcamp de Diseño de Interfaces y CSS.pdf",
-    },
-    {
-        title: "Curso Profesional de TypeScript",
-        platform: "Certificado",
-        year: "2024",
-        icon: "code",
-        certificateUrl: "/certificates/Certificado - Curso Profesional de Typescript.pdf",
-    },
-    {
-        title: "Curso Profesional de Node.js",
-        platform: "Certificado",
-        year: "2024",
-        icon: "node",
-        certificateUrl: "/certificates/Certificado - Curso Profesional de node.js.pdf",
-    },
-    {
-        title: "Curso a fondo de GitHub",
-        platform: "Certificado",
-        year: "2024",
-        icon: "github",
-        certificateUrl: "/certificates/Certificado - Curso a fondo de GitHub.pdf",
-    },
-    {
-        title: "Introducción a las Pruebas de Software (QA)",
-        platform: "Certificado",
-        year: "2024",
-        icon: "code",
-        certificateUrl: "/certificates/Certificado - Curso de Introducción a las Pruebas de Software (QA).pdf",
-    },
-    {
-        title: "Curso de Laravel Profesional",
-        platform: "Certificado",
-        year: "2024",
-        icon: "laravel",
-        certificateUrl: "/certificates/Certificado - Curso de Laravel Profesional.pdf",
-    },
-    {
-        title: "Curso profesional de Flutter",
-        platform: "Certificado",
-        year: "2024",
-        icon: "flutter",
-        certificateUrl: "/certificates/Certificado - Curso profesional de Flutter.pdf",
-    },
-    {
-        title: "Curso profesional de JavaScript",
-        platform: "Certificado",
-        year: "2024",
-        icon: "js",
-        certificateUrl: "/certificates/Certificado - Curso profesional de JavaScript.pdf",
-    },
-    {
-        title: "Curso profesional de React",
-        platform: "Certificado",
-        year: "2024",
-        icon: "react",
-        certificateUrl: "/certificates/Certificado - Curso profesional de React.pdf",
-    },
-    {
-        title: "Cómo Crear un Proyecto de Ingeniería de Datos",
-        platform: "Certificado",
-        year: "2024",
-        icon: "code",
-        certificateUrl: "/certificates/Certificado - Cómo Crear un Proyecto de Ingeniería de Datos.pdf",
-    },
-    {
-        title: "Fundamentos de HTML",
-        platform: "Bootcamp Frontend",
-        year: "2024",
-        icon: "html",
-        certificateUrl: "/certificates/Certificado - Fundamentos de HTML - Bootcamp Frontend, Generación 3.pdf",
-    },
-    {
-        title: "Inteligencia Artificial en Azure",
-        platform: "Bootcamp IA con Azure",
-        year: "2024",
-        icon: "code",
-        certificateUrl: "/certificates/Certificado - Inteligencia Artificial en Azure - Bootcamp IA con Azure.pdf",
-    },
-    {
-        title: "Modelado de microservicios",
-        platform: "Bootcamp de Backend Avanzado",
-        year: "2024",
-        icon: "node",
-        certificateUrl: "/certificates/Certificado - Modelado de microservicios - Bootcamp de Backend Avanzado.pdf",
-    },
-    {
-        title: "Creación de Contenido para YouTube",
-        platform: "Certificado",
-        year: "2020",
-        icon: "code",
-        certificateUrl: "/certificates/diploma-contenido-youtube.pdf",
-    },
-    {
-        title: "Edición con Premiere",
-        platform: "Certificado",
-        year: "2020",
-        icon: "premiere",
-        certificateUrl: "/certificates/diploma-edicion-premiere-2016.pdf",
-    },
-    {
-        title: "Fotografía con Teléfono",
-        platform: "Certificado",
-        year: "2020",
-        icon: "code",
-        certificateUrl: "/certificates/diploma-fotografia-telefono.pdf",
-    },
+  {
+    title: "AWS Community Day Argentina 2025",
+    platform: "AWS Community",
+    year: "2025",
+    icon: "code",
+    certificateUrl:
+      "https://drive.google.com/file/d/1rqPNqPpkri4bNWC2Tluedxjf8WLtN2Gu/view?usp=sharing",
+  },
+  {
+    title: "Diseño web con HTML y CSS",
+    platform: "Certificado",
+    year: "2025",
+    icon: "html",
+    certificateUrl:
+      "https://drive.google.com/file/d/13uAtZx2iSqZg_ypOGHlosW7lozV4yJgJ/view?usp=sharing",
+  },
+  {
+    title: "Introducción a Java",
+    platform: "Certificado",
+    year: "2025",
+    icon: "java",
+    certificateUrl:
+      "https://drive.google.com/file/d/1yE7ZP0ZSnSwXrZbs-nndgeUM2iyXHLqC/view?usp=sharing",
+  },
+  {
+    title: "Introducción a Python",
+    platform: "Certificado",
+    year: "2025",
+    icon: "python",
+    certificateUrl:
+      "https://drive.google.com/file/d/1YnFGgQuzTRg1hfr2I4DegVuI6aixf6Ti/view?usp=sharing",
+  },
+  {
+    title: "Programación con PHP y MySQL",
+    platform: "Certificado",
+    year: "2025",
+    icon: "php",
+    certificateUrl:
+      "https://drive.google.com/file/d/18Bu4MO8FMOpSh1OrgyM2ZMm6G_YnqREe/view?usp=sharing",
+  },
+  {
+    title: "Crear un portafolio para un Científico de Datos",
+    platform: "Bootcamp de Ciencia de Datos",
+    year: "2024",
+    icon: "code",
+    certificateUrl:
+      "https://drive.google.com/file/d/18Z6kllUEIcsYLsWTQOu1Whb_Bjww-hZC/view?usp=sharing",
+  },
+  {
+    title: "Redes Neuronales",
+    platform: "Bootcamp de Machine Learning",
+    year: "2024",
+    icon: "code",
+    certificateUrl:
+      "https://drive.google.com/file/d/1T7CnZdlRnmRMpmfK3B8z2MOXt25qHyk8/view?usp=sharing",
+  },
+  {
+    title: "Animaciones con JavaScript",
+    platform: "Bootcamp",
+    year: "2024",
+    icon: "js",
+    certificateUrl:
+      "https://drive.google.com/file/d/1UkOGFNOcHIuQi9oTKgesw8GRh3e21GXQ/view?usp=sharing",
+  },
+  {
+    title: "Aprendiendo a usar Figma",
+    platform: "Bootcamp de Diseño de Interfaces y CSS",
+    year: "2024",
+    icon: "figma",
+    certificateUrl:
+      "https://drive.google.com/file/d/1gtgPdTI8-nVfMA9pBcTs_wCXJifSGWK8/view?usp=sharing",
+  },
+  {
+    title: "Curso Profesional de TypeScript",
+    platform: "Certificado",
+    year: "2024",
+    icon: "code",
+    certificateUrl:
+      "https://drive.google.com/file/d/1DLCIvExkYH9Zwhf3rB4UvjQV6pjfTJ8G/view?usp=sharing",
+  },
+  {
+    title: "Curso Profesional de Node.js",
+    platform: "Certificado",
+    year: "2024",
+    icon: "node",
+    certificateUrl:
+      "https://drive.google.com/file/d/1uyIl53M5tKBELLMjFLHHUP3lBgClnxhO/view?usp=sharing",
+  },
+  {
+    title: "Curso a fondo de GitHub",
+    platform: "Certificado",
+    year: "2024",
+    icon: "github",
+    certificateUrl:
+      "https://drive.google.com/file/d/1_v82PXnAdKxPfFenQ22IpSfTfrNQBO8e/view?usp=sharing",
+  },
+  {
+    title: "Introducción a las Pruebas de Software (QA)",
+    platform: "Certificado",
+    year: "2024",
+    icon: "code",
+    certificateUrl:
+      "https://drive.google.com/file/d/1yEdiMDi1JcAd9F5Y5vcBu65AJAIZD4jp/view?usp=sharing",
+  },
+  {
+    title: "Curso de Laravel Profesional",
+    platform: "Certificado",
+    year: "2024",
+    icon: "laravel",
+    certificateUrl:
+      "https://drive.google.com/file/d/141KKsSTvkQ_bpRhDhWyVEl6LqHTmDFB-/view?usp=sharing",
+  },
+  {
+    title: "Curso profesional de Flutter",
+    platform: "Certificado",
+    year: "2024",
+    icon: "flutter",
+    certificateUrl:
+      "https://drive.google.com/file/d/14Vn4wmzU9h0B92NZsyB3zoVKsN2hWCIS/view?usp=sharing",
+  },
+  {
+    title: "Curso profesional de JavaScript",
+    platform: "Certificado",
+    year: "2024",
+    icon: "js",
+    certificateUrl:
+      "https://drive.google.com/file/d/13fHRVCPJSFzwFpPYNsgfWMGr7QDsBgFS/view?usp=sharing",
+  },
+  {
+    title: "Curso profesional de React",
+    platform: "Certificado",
+    year: "2024",
+    icon: "react",
+    certificateUrl:
+      "https://drive.google.com/file/d/1AaaQNhvuaqbGZTh6xD7Fu52z532OwTjS/view?usp=sharing",
+  },
+  {
+    title: "Cómo Crear un Proyecto de Ingeniería de Datos",
+    platform: "Certificado",
+    year: "2024",
+    icon: "code",
+    certificateUrl:
+      "https://drive.google.com/file/d/1Eq7zP_UDpLz6E5pSRCNEolLe6bj3QjWq/view?usp=sharing",
+  },
+  {
+    title: "Fundamentos de HTML",
+    platform: "Bootcamp Frontend",
+    year: "2024",
+    icon: "html",
+    certificateUrl:
+      "https://drive.google.com/file/d/1pZnZ-U7BGoVRMLe3XAWFm3uksbobzrMi/view?usp=sharing",
+  },
+  {
+    title: "Inteligencia Artificial en Azure",
+    platform: "Bootcamp IA con Azure",
+    year: "2024",
+    icon: "code",
+    certificateUrl:
+      "https://drive.google.com/file/d/1x3iImPlcRMxLzfYT4BXWPrbYBvPA6RpZ/view?usp=sharing",
+  },
+  {
+    title: "Modelado de microservicios",
+    platform: "Bootcamp de Backend Avanzado",
+    year: "2024",
+    icon: "node",
+    certificateUrl:
+      "https://drive.google.com/file/d/1NGtpBsTFmltG3UDA2hPhZhyg8NxRstqt/view?usp=sharing",
+  },
+  {
+    title: "Creación de Contenido para YouTube",
+    platform: "Certificado",
+    year: "2020",
+    icon: "code",
+    certificateUrl:
+      "https://drive.google.com/file/d/1whdJ5Wuij-fav8MVSVnGF5qRicp61X-b/view?usp=sharing",
+  },
+  {
+    title: "Edición con Premiere",
+    platform: "Certificado",
+    year: "2020",
+    icon: "premiere",
+    certificateUrl:
+      "https://drive.google.com/file/d/1wbKs-ev8AzyTrW1yGBeVIPk5BuOJseHf/view?usp=sharing",
+  },
+  {
+    title: "Fotografía con Teléfono",
+    platform: "Certificado",
+    year: "2020",
+    icon: "code",
+    certificateUrl:
+      "https://drive.google.com/file/d/1BjoDoIIanVyLhHAJ3T_3TwS9PSUCBiDQ/view?usp=sharing",
+  },
 ];
